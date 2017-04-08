91744b85ce68cc62cf39e10e021a1f9d9e550a06
poojithaamin <poojaamin87@gmail.com>
2017-04-07 22:07:58 -0700

    Update UmlParser.java

--- src/main/java/UmlParser.java
+++ src/main/java/UmlParser.java
@@ -6,16 +6,20 @@ public class UmlParser {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println(args[0]);
-        if (args.length == 2) {
+        if (args.length == 3) {
         		File srcFolder = new File(args[0]);
         		if(!srcFolder.exists()) {
                 	System.err.println ("Invalid folder");
                 	System.exit(0);
         		}
         		else {
-                	System.out.println(+','+args[1]);
-                	UmlClassParser jp = new UmlClassParser(srcFolder,args[1]);
-                	jp.parse();
+				System.out.println(+','+args[1]);
+				if (args[2]=="class"){ 	
+					UmlClassParser jp = new UmlClassParser(srcFolder,args[1]);
+					jp.parse();	
+			        }
+				else if(args[2]=="sequence"){
+			    }	                	
                 }
         }        		
         else {
